af88f6b2ce6af474600ebf2b09c4dce82ab85c28
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2017-12-04 10:17:52 -0600

    Added password system.

--- DES_Decryption.py
+++ DES_Decryption.py
@@ -1,10 +1,68 @@
-from Crypto.Cipher import DES
-text = eval(input())
+from Crypto.Cipher import DES  # Imports DES for decryption
+import bcrypt  # Imports the module used for hashing passwords
+# Sets the username to nothing
+username = ''
+# Tries the code
+try:
+    # Opens the username and password files for reading
+    fileu = open("dec_username.txt", 'r')
+    filep = open("dec_password.txt", 'r')
+    # Reads in the username and password hash
+    username = fileu.read()
+    hashed = eval(filep.read())
+    # Closes both files
+    fileu.close()
+    filep.close()
+    # Receives the input of a password and encodes it into a byte string
+    password = input("Please enter in existing password: ").encode('utf-8')
+    # Sets the loop condition for number of times
+    num = 0
+    # While the password hash of the entered password does not equal
+    # the existing password hash and the number of times tried is not 5
+    while bcrypt.hashpw(password, hashed) != hashed and num < 5:
+        # Receive password input after failed try
+        password = input("Incorrect password. Please try again: ")
+        num = num+1
+    # If the number of times has reached 5, print the
+    # limit statement
+    if num == 5:
+        print("Sorry, you have surpassed the limit.")
+    # Prints the success statement
+    else:
+        print("You are now logged in.")
+
+# If the file is not found
+except FileNotFoundError:
+    # Automatically creates a username
+    username = "exists"
+    # Opens the username and password files for write mode
+    fileu = open("dec_username.txt", 'w')
+    filep = open("dec_password.txt", 'w')
+    # Writes username to file and closes the file
+    fileu.write(username)
+    fileu.close()
+    # Accepts the input of a new password encoded the byte string
+    pword = input("Please enter a new password: ").encode('utf-8')
+    # Assigns the password hash to the variable with salt
+    hashed = bcrypt.hashpw(pword, bcrypt.gensalt())
+    # Writes string version to password file
+    filep.write(str(hashed))
+    # Closes file
+    filep.close()
+    # Prints the success statement
+    print("You are now logged in.")
+# Receives the cipher text from user input
+text = eval(input("Please enter in the encrypted DES message: "))
 
 def decrypt_message(cipher_text):
+    """Decrypts the DES cipher text"""
+    # Creates the DES object
     obj = DES.new("alphabet")
-    print(cipher_text)
+    # Prints the cipher text out
+    print("Ciphertext: "+cipher_text)
+    # Assigns the plain text to the variable
     plaintext = obj.decrypt(cipher_text)
-    print(plaintext)
+    # Prints out the plain text
+    print("Plaintext: "+plaintext)
 
 decrypt_message(text)
--- DES_Email Algorithm.py
+++ DES_Email Algorithm.py
@@ -1,8 +1,10 @@
-import smtplib # Import statement for actual sending function
-from Crypto.Cipher import DES
-def prompt(prompt):
-    return input(prompt).strip()
+import smtplib  # Import statement for actual sending function
+from Crypto.Cipher import DES  # Import statement for DES Encryption
 
+def prompt(prompt):
+    """Returns the input of the prompt."""
+    return input(prompt).strip()
+# The inputs for all the necessary email parts
 fromAddr = prompt("From: ")
 toAddrs = prompt("To: ").split()
 ccAddr = prompt("CC: ").split()
@@ -11,44 +13,62 @@ message = prompt("Enter message: ")
 login = prompt("Login Username: ")
 password = prompt("Password: ")
 
-# print("Enter message, end with ^D (Unix) or ^Z (Windows):")
-
 
 def sendemail(from_addr, to_addrs, cc_addr, subject, msg, log_in, pass_word, smtpserver='smtp.gmail.com:465'):
+    """Sends the email with all the correct configurations"""
+    # Creates the headers for sending out the email
     header = 'From: %s\n' % from_addr
     header += 'To: %s\n' % ','.join(to_addrs)
     header += 'Cc: %s\n' % ','.join(cc_addr)
     header += 'Subject: %s\n\n' % subject
     msg = header + msg
 
+    # Creates the server specifications
     server_ssl = smtplib.SMTP_SSL(smtpserver)
     server_ssl.ehlo()
     server_ssl.login(log_in,pass_word)
     problems = server_ssl.sendmail(from_addr, to_addrs, msg)
     server_ssl.quit()
+    # Returns any recorded problems with the system
     return problems
-def des_encrypt(message):
-    message_edit = ""
-    m = 0
-    for i in range(0, len(message)):
 
+def des_encrypt(message):
+    """Encrypts the email message with DES"""
+    # Variable for the edited version of the message (removes spaces)
+    message_edit = ""
+    # The initial index for the message segment
+    m = 0
+    # Loops through the message and deletes teh spaces
+    for i in range(0, len(message)):
+        # Checks if the character in the message is a space
         if message[i].isspace():
+            # Creates a new segment of the edited message
+            # Increments the starting index of the message segment
             message_edit = message_edit + message[m:i]
             m = i+1
+        # Sets the message_edit equal to the message if the
+        # if the original message has no spaces
         elif i == len(message)-1 and message_edit == "":
             message_edit = message
 
-
+    # Adds filler characters if the message is not
+    # a multiple of 8
     while len(message) % 8 != 0:
         message = message + "&"
 
-    print(message_edit)
+    # Prints the edited message
+    print("Formatted message: "+message_edit)
+    # Creates a new DES object with the given key
     obj = DES.new("alphabet")
+    # Assigns created cipher to the variable
     cypher = obj.encrypt(message)
-
+    # Returns the new cipher
     return cypher
+# Assigns the created cipher to the variable
 cypher = des_encrypt(message)
-print(cypher)
+# This prints the newly created cipher
+print("Ciphertext: "+cypher)
+# Converts the cypher to a string for transportation
 cypher = str(cypher)
-
+# Sends out the email with the cipher
 sendemail(fromAddr, toAddrs, ccAddr, subject, cypher, login, password)
